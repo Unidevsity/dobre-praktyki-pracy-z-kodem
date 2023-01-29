095d9cc1c5e01dcba8b906e3e1d21a66865bd21f
Rafal Piekara <rafal@piekara.me>
2023-01-29 01:42:55 +0100

    Add active record transaction

--- app/services/register_user.rb
+++ app/services/register_user.rb
@@ -1,9 +1,10 @@
 class RegisterUser
 
   def call
-    user = User.new(email: 'rafal@grubykodzi.pl')
-
-    user.save
-    Notification.create(message: "User #{user.email} has been created")
+    ActiveRecord::Base.transaction do
+      user = User.new(email: 'rafal@grubykodzi.pl')
+      user.save!
+      Notification.create(message: "User #{user.email} has been created")
+    end
   end
 end
